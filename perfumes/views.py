e0aeed2625d92bb934654611cdeefdeb467da92e
Alxjandrz <omarubio2404@gmail.com>
2026-05-11 18:13:03 -0700

    agregue views.py de perfumes, puse lista de perfumes y sus detalles velarde

--- perfumes/views.py
+++ perfumes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Perfume, Marca
 
-# Create your views here.
+def lista_perfumes(request):
+    perfumes = Perfume.objects.all()
+    return render(request, 'perfumes/lista.html', {'perfumes': perfumes})
+
+def detalle_perfume(request, pk):
+    perfume = get_object_or_404(Perfume, pk=pk)
+    return render(request, 'perfumes/detalle.html', {'perfume': perfume})
