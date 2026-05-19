a02d6804a0d30c33004e154b48e70628507b658f
Alxjandrz <omarubio2404@gmail.com>
2026-05-18 18:03:30 -0700

    Arregle errores en el funcionaminto y agregue los carritos

--- perfumes/admin.py
+++ perfumes/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Perfume, Marca
+from .models import Marca, Perfume, Coleccion, Resena, Carrito, CarritoItem
+
 
 admin.site.register(Marca)
-admin.site.register(Perfume)
+admin.site.register(Coleccion)
+admin.site.register(Resena)
+admin.site.register(Carrito)
+admin.site.register(CarritoItem)
+
+
+@admin.register(Perfume)
+class PerfumeAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'marca', 'precio', 'ml', 'genero', 'categoria', 'en_stock')
+
+    list_filter = ('categoria', 'genero', 'en_stock', 'marca')
+
+    search_fields = ('nombre', 'marca__nombre')
