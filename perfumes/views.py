9e918fa8599892289a8d078e6e714892543cfc2a
Alxjandrz <omarubio2404@gmail.com>
2026-05-27 17:59:17 -0700

    Modifique en views una categoria nueva, tambien para poner imagenes

--- perfumes/views.py
+++ perfumes/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Perfume, Marca
+from .models import Perfume, Coleccion, Resena
+
 
 def lista_perfumes(request):
     disenador = Perfume.objects.filter(categoria='D')
     arabes = Perfume.objects.filter(categoria='A')
+    nicho = Perfume.objects.filter(categoria='N')
     return render(request, 'perfumes/lista.html', {
         'disenador': disenador,
         'arabes': arabes,
+        'nicho': nicho,
     })
+
 
 def detalle_perfume(request, pk):
     perfume = get_object_or_404(Perfume, pk=pk)
-    return render(request, 'perfumes/detalle.html', {'perfume': perfume})
+    resenas = perfume.resenas.all().order_by('-fecha')
+    colecciones = perfume.colecciones.all()
+    return render(request, 'perfumes/detalle.html', {
+        'perfume': perfume,
+        'resenas': resenas,
+        'colecciones': colecciones,
+    })
